27972a224daa36d03e50e7b226b6219595323a30
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-09-11 10:16:35 +0300

    finished defining the 2 relatonships

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from xmlrpc.client import Boolean
+from sqlalchemy import ForeignKey, Column, Integer, String, Boolean, MetaData, create_engine
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,4 +8,55 @@ convention = {
 }
 metadata = MetaData(naming_convention=convention)
 
+engine = create_engine('sqlite:///migrations_test.db')
+
 Base = declarative_base(metadata=metadata)
+
+class Audition(Base):
+    __tablename__ = 'auditions'
+
+    id = Column(Integer(), primary_key = True)
+    actor = Column(String())
+    location = Column(String())
+    phone = Column(Integer())
+    hired = Column(Boolean())
+    role_id = Column(Integer(), ForeignKey('roles.id'))
+
+    roles = relationship("Role", back_populates='auditions')
+
+    def call_back(self):
+        self.hired = True
+
+    def __repr__(self):
+        return f"Actor {self.actor}: "\
+            +f"{self.location},"\
+            +f"{self.phone},"\
+            +f"{self.hired}"
+    
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    charater_name = Column(String())
+
+    auditions = relationship('Audition', back_populates='roles')
+    
+    def lead(self):
+        for audition in self.auditions:
+            if audition.hired:
+                return audition      
+        return 'no actor has been hired for this role'
+    
+    def understudy(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        if len(hired_auditions) >= 2:
+            return hired_auditions[1]
+        return 'no actor has been hired for understudy for this role'
+
+    def __repr__(self):
+        return f"Role {self.charater_name}: "
+    
+
+role = Role()
+role.charater_name = "cheese"
+print(role)
